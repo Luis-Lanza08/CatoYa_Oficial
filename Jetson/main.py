721f95bb96e6e1a1de85bbd911a13c67c65b4005
Luis-Lanza08 <luis.lanza@ucb.edu.bo>
2023-06-08 09:38:43 -0400

    mostrar errror en pantalla

--- Jetson/main.py
+++ Jetson/main.py
@@ -13,7 +13,8 @@ i = 0 # valor integral
 p_anterior = 0 # error anterior
 control = 0 #
 
-
+fila_blancos = 450
+control_err = 0 # inicializar variable
 while True:
     isTrue, frame = capture.read() # leer frames
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) # convertir a escala de grises
@@ -22,9 +23,9 @@ while True:
     anchura = frame.shape[1]
     threshold, thresh_inv = cv.threshold(gray, 100, 255, cv.THRESH_BINARY_INV ) #Binarizar imagen
     thresh_inv = cv.cvtColor(thresh_inv, cv.COLOR_GRAY2RGB) #Volver a convetir a color para poder ver la lineas se utiliza el inverso para que los negros se hagan blancos
-    blancosEnFila = thresh_inv[100].astype(np.int32) # detectar blancos en una fila
+    blancosEnFila = thresh_inv[fila_blancos].astype(np.int32) # detectar blancos en una fila
     x = np.arange(len(blancosEnFila)) # leer tamanio del array
-
+    
     ## detectar el camino 
     if(255 in blancosEnFila):  ## si detecta blanco
         bordes = np.where(blancosEnFila == 255)[0]  # detectar bordes del camino
@@ -38,11 +39,12 @@ while True:
 
 
         final = thresh_inv.copy()
-
-
+        cv.line(final,(0,fila_blancos), (1000,fila_blancos), (0,0,255), thickness=1) # recta para detectar blancos
         cv.line(final, (min,0), (min,500),(0,255,0), thickness=1) # graficar recta en lacalle
         cv.line(final, (max,0), (max,500),(0,255,0), thickness=1) # graficar recta en la calle
         cv.line(final,(medio,0), (medio,500),(0,255,0), thickness=1) # graficar recta en el centro de la calle
+        cv.putText(final, str(control_err), (50,50), cv.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), thickness = 1)
+
         cv.imshow('detectar', final)
 
         # PID
